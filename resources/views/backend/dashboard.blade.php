82bb803dc6f3d8878bb5d0489536c5411ce0efcf
valiantboymaksud <valiantmaksud@gmail.com>
2021-12-03 22:28:22 +0600

    update

--- resources/views/backend/dashboard.blade.php
+++ resources/views/backend/dashboard.blade.php
@@ -83,18 +83,7 @@
                     </div>
                 </div>
             </div>
-            <div class="col-md-4 grid-margin stretch-card">
-                <div class="card">
-                    <div class="card-body d-flex flex-column">
-                        <div class="wrapper">
-                            <h4 class="card-title mb-0">Net Profit Margin</h4>
-                            <p>Started collecting data from February 2019</p>
-                            <div class="mb-4" id="net-profit-legend"></div>
-                        </div>
-                        <canvas class="my-auto mx-auto" height="250" id="net-profit"></canvas>
-                    </div>
-                </div>
-            </div>
+
         </div>
 
     </div>
